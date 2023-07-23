db011ee48d647202e75e04ae5b4048bc2d6d5c52
rohitcodism <rohitpaula18i@gmail.com>
2023-07-23 15:50:40 +0530

    do it

--- cbr2.cpp
+++ cbr2.cpp
@@ -7,14 +7,16 @@ void swap(int* a, int* b){
     *b = temp;
 }
 
-void swapRefVar(){
-    
+void swapRefVar(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main(){
     int x,y;
     cout<<"Enter any two no.s : ";
     cin>>x>>y;
-    swap(&x, &y);
+    swapRefVar(x, y);
     cout<<x<<y<<endl;
 }
