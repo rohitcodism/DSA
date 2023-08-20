4b109961d465d7e0b83ebae2ac19381a891abbd1
rohitcodism <rohitpaula18i@gmail.com>
2023-08-20 20:12:38 +0530

    do it

--- GFG_Problems/d3p3.cpp
+++ GFG_Problems/d3p3.cpp
@@ -1,5 +1,5 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 /* Link list Node */
@@ -29,9 +29,6 @@ struct Node *start = NULL;
 };
 
 */
-class Solution
-{
-    public:
     //Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node *head) {
         
@@ -55,40 +52,10 @@ class Solution
         }
         return head;
     }
-};
 
 
-//{ Driver Code Starts.
 
-// Function to sort a linked list of 0s, 1s and 2s
-void printList(struct Node *Node) {
-    while (Node != NULL) {
-        printf("%d ", Node->data);
-        Node = Node->next;
-    }
-    printf("\n");
-}
 
-/* Drier program to test above function*/
-void insert(int n1) {
-    int n, value, i;
-    // scanf("%d",&n);
-    n = n1;
-    struct Node *temp;
-    for (i = 0; i < n; i++) {
-        scanf("%d", &value);
-
-        if (i == 0) {
-            start = new Node(value);
-            temp = start;
-            continue;
-        } else {
-            temp->next = new Node(value);
-            temp = temp->next;
-            temp->next = NULL;
-        }
-    }
-}
 
 int main() {
 
