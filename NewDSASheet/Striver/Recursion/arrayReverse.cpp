bb4655ee6d664d59d97abedf3dcc6e47c09af567
rohitcodism <rohitpaula18i@gmail.com>
2024-03-01 07:55:27 +0530

    Fix arrayReverse function and add new revArray2 function

--- NewDSASheet/Striver/Recursion/arrayReverse.cpp
+++ NewDSASheet/Striver/Recursion/arrayReverse.cpp
@@ -1,14 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void revArray(int a[], int lb, int ub){
+void revArray(int a[], int lb, int ub){  //* Using two parameter
     
-    if(lb == ub || lb > ub){
+    if(lb >= ub){
         return;
     }
 
     swap(a[lb], a[ub]);
     revArray(a,lb+1,ub-1);
+}
+
+void revArray2(int a[], int lb){    //* Using One parameter
+
+    int n = 6;
+
+    if(lb>=n-lb-1){
+        return;
+    }
+    swap(a[lb], a[n-lb-1]);
+    revArray2(a,lb+1);
 }
 
 void displayArray(int arr[], int n){
@@ -20,11 +31,11 @@ void displayArray(int arr[], int n){
 
 int main(){
 
-    int arr[5] = {1,2,3,4,5};
+    int arr[6] = {1,2,3,4,5,6};
 
-    revArray(arr,0,4);
+    revArray2(arr,0);
 
-    displayArray(arr,5);
+    displayArray(arr,6);
 
     return 0;
 }
