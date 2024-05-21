05b5cc6a8e6242145272d3e1a482a09181d8df19
rohitcodism <rohitpaula18i@gmail.com>
2024-05-21 09:40:35 +0530

    feat: Update Flloyd-Warshall.cpp to handle infinite distances

--- NewDSASheet/My Problems/Flloyd-Warshall.cpp
+++ NewDSASheet/My Problems/Flloyd-Warshall.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define INF 999;
 
 int main()
 {
     int n;
-    cout << "Enter the number of vertices in the graph : ";
+    cout << "Enter the number of vertices in the graph: ";
     cin >> n;
     cout << endl;
 
-    int **graph = new int *[n];
+    vector<vector<int>> graph(n, vector<int>(n));
 
     cout << "Enter the adjacency matrix: " << endl;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        graph[i] = new int[n];
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> graph[i][j];
+
+            if (graph[i][j] == 999)
+            {
+                graph[i][j] = 1e9;
+            }
         }
     }
 
-    for (int k = 1; k <= n; k++)
+    for (int k = 0; k < n; k++)
     {
-        for (int i = 1; i <= n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 1; j <= n; j++)
+            for (int j = 0; j < n; j++)
             {
                 graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
             }
         }
     }
 
-    cout << "All the shortest path : " << endl;
-    for (int i = 1; i <= n; i++)
+    cout << "All the shortest paths: " << endl;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<graph[i][j]<<"  ";
+            if (graph[i][j] == 1e9)
+            {
+                graph[i][j] = INF;
+            }
+            cout << graph[i][j] << "  ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
