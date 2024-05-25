2d1a0d4f9a815bd0745898f0bf29541f4266596d
rohitcodism <rohitpaula18i@gmail.com>
2024-05-25 16:28:31 +0530

    feat: Refactor Floyd-Warshall algorithm for all-pairs shortest path

--- NewDSASheet/My Problems/FLW.cpp
+++ NewDSASheet/My Problems/FLW.cpp
@@ -1,55 +1,93 @@
 #include <iostream>
-#include <vector>
-#include <bits/stdc++.h>
+#include <climits> // For INT_MAX
+#include <fstream>
 using namespace std;
 
-#define INF 999;
+#define INF 999
+#define MAX 100
 
-int main()
-{
-    int n;
-    cout << "Enter the number of vertices in the graph: ";
-    cin >> n;
-    cout << endl;
-
-    vector<vector<int>> graph(n, vector<int>(n));
-
-    cout << "Enter the adjacency matrix: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> graph[i][j];
-
-            if (graph[i][j] == 999)
-            {
-                graph[i][j] = 1e9;
+void initializeGraph(int** graph, int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (graph[i][j] == INF) {
+                graph[i][j] = INT_MAX;
             }
         }
     }
+}
 
-    for (int k = 0; k < n; k++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+void floydWarshall(int** graph, int n) {
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (graph[i][k] < INT_MAX && graph[k][j] < INT_MAX) { // To avoid overflow
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
             }
         }
     }
+}
 
+void printGraph(int** graph, int n) {
     cout << "All the shortest paths: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (graph[i][j] == 1e9)
-            {
-                graph[i][j] = INF;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (graph[i][j] == INT_MAX) {
+                cout << "INF  ";
+            } else {
+                cout << graph[i][j] << "  ";
             }
-            cout << graph[i][j] << "  ";
         }
         cout << endl;
     }
+}
+
+int main() {
+    ifstream inputFile("FLW.txt");
+    if (!inputFile) {
+        cout << "Error opening input file." << endl;
+        return 1;
+    }
+
+    int n, e;
+    inputFile >> n >> e;
+
+    int** graph = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        graph[i] = new int[n];
+    }
+
+    // Initialize the graph with infinity
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == j) {
+                graph[i][j] = 0;
+            } else {
+                graph[i][j] = INT_MAX;
+            }
+        }
+    }
+
+    for (int i = 0; i < e; ++i) {
+        int u, v, w;
+        inputFile >> u >> v >> w;
+        graph[u][v] = w;
+        graph[v][u] = w; // For undirected graphs, mirror the edge
+    }
+
+    inputFile.close();
+
+    initializeGraph(graph, n);
+    floydWarshall(graph, n);
+    printGraph(graph, n);
+
+    // Free dynamically allocated memory
+    for (int i = 0; i < n; ++i) {
+        delete[] graph[i];
+    }
+    delete[] graph;
+
+    return 0;
 }
